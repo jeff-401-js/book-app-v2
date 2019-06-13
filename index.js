4fc2130d8a14b2c7cb8c9effd64486caf90c3096
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-12 18:05:50 -0700

    begin refactor after demo

--- index.js
+++ index.js
@@ -3,3 +3,11 @@
 require('dotenv').config();
 
 require('./server/server.js').start(process.env.PORT);
+
+
+// 'use strict';
+
+// require('dotenv').config();
+
+// const {server} = require('./src/server.js');
+// server.start(process.env.PORT);
